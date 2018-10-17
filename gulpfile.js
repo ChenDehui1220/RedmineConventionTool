0502f057fffcf84bcf1448e7e289b69abfdf7e15
gary_chen <gary_chen@friday.tw>
2018-10-17 22:29:45 +0800

    add new tag

--- gulpfile.js
+++ gulpfile.js
@@ -3,88 +3,95 @@
  *  @2016/11/11
  */
 
-'use strict';
+'use strict'
 
 // dependencies
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var uglify = require('gulp-uglify');
-var minicss = require('gulp-clean-css');
-var minijson = require('gulp-jsonminify');
-var htmlmin = require('gulp-htmlmin');
-var del = require('del');
-var runSequence = require('run-sequence');
+var gulp = require('gulp')
+var gutil = require('gulp-util')
+var uglify = require('gulp-uglify')
+var minicss = require('gulp-clean-css')
+var minijson = require('gulp-jsonminify')
+var htmlmin = require('gulp-htmlmin')
+var del = require('del')
+var runSequence = require('run-sequence')
 
-var srcFolder = './src/';
-var buildFolder = './build/';
+var srcFolder = './src/'
+var buildFolder = './build/'
 
 // 清空Dist
 gulp.task('clean', function() {
-    return del([buildFolder + '*'], {
-        force: true
-    });
-});
+  return del([buildFolder + '*'], {
+    force: true
+  })
+})
 
 // 複製
 gulp.task('copy', function() {
-    return gulp.src([srcFolder + '*.png'])
-        .pipe(gulp.dest(buildFolder))
-        .on('error', gutil.log);
-});
+  return gulp
+    .src([srcFolder + '*.png'])
+    .pipe(gulp.dest(buildFolder))
+    .on('error', gutil.log)
+})
 
 // 壓縮 HTML
 gulp.task('minify-html', function() {
-    return gulp.src(srcFolder + 'popup.html')
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(gulp.dest(buildFolder))
-        .on('error', gutil.log);
-});
+  return gulp
+    .src(srcFolder + 'popup.html')
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        removeComments: true
+      })
+    )
+    .pipe(gulp.dest(buildFolder))
+    .on('error', gutil.log)
+})
 
 // 壓縮 JS
 gulp.task('minify-js', function() {
-    return gulp.src(srcFolder + '*.js')
-        .pipe(uglify())
-        .pipe(gulp.dest(buildFolder))
-        .on('error', gutil.log);
-});
+  return gulp
+    .src(srcFolder + '*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest(buildFolder))
+    .on('error', gutil.log)
+})
 
 // 壓縮 CSS
 gulp.task('minify-css', function() {
-    return gulp.src(srcFolder + '*.css')
-        .pipe(minicss())
-        .pipe(gulp.dest(buildFolder))
-        .on('error', gutil.log);
-});
+  return gulp
+    .src(srcFolder + '*.css')
+    .pipe(minicss())
+    .pipe(gulp.dest(buildFolder))
+    .on('error', gutil.log)
+})
 
 // 壓縮 JSON
 gulp.task('minify-json', function() {
-    return gulp.src(srcFolder + 'manifest.json')
-        .pipe(minijson())
-        .pipe(gulp.dest(buildFolder))
-        .on('error', gutil.log);
-});
+  return gulp
+    .src(srcFolder + 'manifest.json')
+    .pipe(minijson())
+    .pipe(gulp.dest(buildFolder))
+    .on('error', gutil.log)
+})
 
 // 監控 JS
 gulp.task('watch', function(file) {
-    gulp.watch(srcFolder + '*.js', ['minify-js']);
-    gulp.watch(srcFolder + '*.css', ['minify-css']);
-    gulp.watch(srcFolder + '*.html', ['minify-html']);
-});
+  gulp.watch(srcFolder + '*.js', ['minify-js'])
+  gulp.watch(srcFolder + '*.css', ['minify-css'])
+  gulp.watch(srcFolder + '*.html', ['minify-html'])
+})
 
 // 佈署
 gulp.task('deploy', function() {
-    runSequence(
-        'clean',
-        'copy',
-        'minify-html',
-        'minify-js',
-        'minify-css',
-        'minify-json',
-        function() {
-            gutil.log('build completed!');
-        }
-    );
-});
+  runSequence(
+    'clean',
+    'copy',
+    'minify-html',
+    'minify-js',
+    'minify-css',
+    'minify-json',
+    function() {
+      gutil.log('build completed!')
+    }
+  )
+})
